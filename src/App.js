f9f3ff13aea4f9a8ba775557317d5c889d327ca8
Ola Assem <ola.assem@viget.com>
2021-07-13 20:53:13 +0200

    apptinfo comp and passong appt props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BiCalendar, BiTrash } from 'react-icons/bi';
+import { BiCalendar } from 'react-icons/bi';
 import AddAppt from './components/AddAppt';
+import ApptInfo from './components/ApptInfo';
 import Search from './components/Search';
 import data from './data.json'
 
@@ -14,23 +15,7 @@ function App() {
       <Search />
 
       <ul className="divide-y divide-gray-200">
-        {data.map(appt => {
-          return (
-            <li className="px-3 py-3 flex items-start">
-              <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <BiTrash /></button>
-              <div className="flex-grow">
-                <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{appt.petName}</span>
-                  <span className="flex-grow text-right">{appt.aptDate}</span>
-                </div>
-                <div><b className="font-bold text-blue-500">Owner:</b> {appt.ownerName}</div>
-                <div className="leading-tight">{appt.aptNotes}</div>
-              </div>
-            </li>
-          )
-        })}
+        {data.map( appt => <ApptInfo key={appt.id} appt={appt} /> )}
       </ul>
     </div>
   );
---
+++ src/components/ApptInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BiTrash } from 'react-icons/bi';
+
+const ApptInfo = ({appt}) => {
+  return (
+    <li key={appt.id} className="px-3 py-3 flex items-start">
+      <button type="button"
+        className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        <BiTrash /></button>
+      <div className="flex-grow">
+        <div className="flex items-center">
+          <span className="flex-none font-medium text-2xl text-blue-500">{appt.petName}</span>
+          <span className="flex-grow text-right">{appt.aptDate}</span>
+        </div>
+        <div><b className="font-bold text-blue-500">Owner:</b> {appt.ownerName}</div>
+        <div className="leading-tight">{appt.aptNotes}</div>
+      </div>
+    </li>
+  )
+}
+
+export default ApptInfo;
