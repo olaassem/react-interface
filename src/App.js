d9e7ec3d7fe0c360a969f6741e0829954a39b6aa
Ola Assem <ola.assem@viget.com>
2021-07-13 17:26:12 +0200

    project comps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { BiArchive } from 'react-icons/bi';
+import { BiCalendar } from 'react-icons/bi';
+import AddAppt from './components/AddAppt';
 import Search from './components/Search';
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-blue-800 align-top mr-4" /> 
+      <h1 className="text-5xl mb-8">
+        <BiCalendar className="inline-block align-top mr-4" /> 
         My Appointments
       </h1>
+      <AddAppt />
       <Search />
     </div>
   );
