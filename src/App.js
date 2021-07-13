58e60bee37b6e2d952cf6c6076dd46d87d6661e0
Ola Assem <ola.assem@viget.com>
2021-07-13 17:15:04 +0200

    adds Search comp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BiArchive } from 'react-icons/bi';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <header className="App-header">
-        <h1 className="text-5xl">
-          <BiArchive className="inline-block text-blue-800 align-top mr-4" /> 
-          My Appointments
-        </h1>
-      </header>
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-blue-800 align-top mr-4" /> 
+        My Appointments
+      </h1>
+      <Search />
     </div>
   );
 }
