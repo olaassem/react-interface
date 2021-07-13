086ac1df730bc58d2fcf62a43842a6acb3e49603
Ola Assem <ola.assem@viget.com>
2021-07-13 17:06:55 +0200

    installs tailwindcss & craco + config files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { BiArchive } from 'react-icons/bi';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto mt-3 font-thin">
       <header className="App-header">
-        <h1><BiArchive /> My Appointments</h1>
+        <h1 className="text-5xl">
+          <BiArchive className="inline-block text-blue-800 align-top mr-4" /> 
+          My Appointments
+        </h1>
       </header>
     </div>
   );
