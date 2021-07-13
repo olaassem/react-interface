fac7408257c32add7eb722993ecad0c95e1885de
Ola Assem <ola.assem@viget.com>
2021-07-13 20:39:15 +0200

    imports json data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BiCalendar } from 'react-icons/bi';
+import { BiCalendar, BiTrash } from 'react-icons/bi';
 import AddAppt from './components/AddAppt';
 import Search from './components/Search';
+import data from './data.json'
 
 function App() {
   return (
@@ -11,6 +12,26 @@ function App() {
       </h1>
       <AddAppt />
       <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {data.map(appt => {
+          return (
+            <li className="px-3 py-3 flex items-start">
+              <button type="button"
+                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <BiTrash /></button>
+              <div className="flex-grow">
+                <div className="flex items-center">
+                  <span className="flex-none font-medium text-2xl text-blue-500">{appt.petName}</span>
+                  <span className="flex-grow text-right">{appt.aptDate}</span>
+                </div>
+                <div><b className="font-bold text-blue-500">Owner:</b> {appt.ownerName}</div>
+                <div className="leading-tight">{appt.aptNotes}</div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
